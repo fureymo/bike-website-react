42e98dd32dcb1e6b7b1732afc7536ab70207466c
fureymo <pard.gee@gmail.com>
2019-11-12 18:24:58 +0700

    seaction Header edited

--- src/sections/Header/Header.js
+++ src/sections/Header/Header.js
@@ -7,16 +7,16 @@ function Header() {
         <div className="Header">
             <div className="Header__top">
                 <ul className="Header__left-params">
-                   <li>Call</li>
+                   <li>usd</li>
                    <li>English</li>
                    <li>Call us: (012) 3456 7890</li>
                 </ul>
                 <ul className="Header__nav">
-                    <li>My Account</li>
-                    <li>Wishlist</li>
-                    <li>My Cart</li>
-                    <li>Checkout</li>
-                    <li>Login</li>
+                    <a href="#" className="Header__nav-item"><li>My Account</li></a>
+                    <a href="#" className="Header__nav-item"><li>Wishlist</li></a>
+                    <a href="#" className="Header__nav-item"><li>My Cart</li></a>
+                    <a href="#" className="Header__nav-item"><li>Checkout</li></a>
+                    <a href="#" className="Header__nav-item Header__nav-item_noborder"><li>Login</li></a>
                 </ul>
             </div>
         </div>
