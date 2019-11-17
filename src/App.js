d1ee0015d80466728d8d240ce6ffdff3e2dd1e63
fureymo <pard.gee@gmail.com>
2019-11-17 16:25:00 +0700

    Revert "Revert "added Logosection""
    This reverts commit 2a5f36eaaebe648418f63df5b13229a0eef77e64.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import './fonts/fonts.scss';
 
 import Header from './sections/Header/Header';
+import LogoSection from './sections/LogoSection/LogoSection';
 
 function App() {
   return (
     <div className="App">
       <Header/>
+      <LogoSection/>
     </div>
   );
 }
