eeadd1f234d8379d5bfff10f0c9d88530ff5941a
fureymo <pard.gee@gmail.com>
2019-12-06 20:27:08 +0700

    Finished AdvantagesSection

--- src/sections/AdvantagesSection/AdvantagesSection.js
+++ src/sections/AdvantagesSection/AdvantagesSection.js
@@ -4,19 +4,22 @@ import './AdvantagesSection.scss';
 
 function AdvantagesSection() {
     return (
-        <section  className="AdvantagesSection">
-            <div className="AdvantagesSection__item">
-                <h5 className="AdvantagesSection__subtitle">free uk delivery</h5>
-                <div>International delivery available</div>
+        <section  className="AdvantagesSection-container">
+            <div className="AdvantagesSection">
+                <div className="AdvantagesSection__item">
+                    <h5 className="AdvantagesSection__subtitle">free uk delivery</h5>
+                    <div className="AdvantagesSection__description">International delivery available</div>
+                </div>
+                <div className="AdvantagesSection__item">
+                    <h5 className="AdvantagesSection__subtitle">gift cards & wrapping</h5>
+                    <div className="AdvantagesSection__description">The perfect way to bring a smile</div>
+                </div>
+                <div className="AdvantagesSection__item">
+                    <h5 className="AdvantagesSection__subtitle">qualified opticians</h5>
+                    <div className="AdvantagesSection__description">Prescription Experts</div>
+                </div>
             </div>
-            <div className="AdvantagesSection__item">
-                <h5 className="AdvantagesSection__subtitle">gift cards & wrapping</h5>
-                <div>The perfect way to bring a smile</div>
-            </div>
-            <div className="AdvantagesSection__item">
-                <h5 className="AdvantagesSection__subtitle">qualified opticians</h5>
-                <div>Prescription Experts</div>
-            </div>
+
         </section>
     );
 };
