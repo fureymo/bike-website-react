ea00ed639152f402e880d8cb22fa4b0101aef135
fureymo <pard.gee@gmail.com>
2020-01-26 16:18:39 +0700

    use react class components instead of functional components

--- src/sections/DealSection/DealSection.js
+++ src/sections/DealSection/DealSection.js
@@ -2,80 +2,82 @@
 import React from 'react';
 import './DealSection.scss';
 
-function DealSection() {
-    return (
-        <section className="DealSection">
-            <div className="DealSection__image"/>
-            <div className="DealSection__info-block">
-                <ul className="DealSection__timer-list">
-                    <li className="DealSection__timer-item">
-                        <div className="DealSection__timer-number">120</div>
-                        <div className="DealSection__timer-period">days</div>
-                    </li>
-                    <li className="DealSection__timer-item">
-                        <div className="DealSection__timer-number">09</div>
-                        <div className="DealSection__timer-period">hour</div>
-                    </li>
-                    <li className="DealSection__timer-item">
-                        <div className="DealSection__timer-number">38</div>
-                        <div className="DealSection__timer-period">min</div>
-                    </li>
-                    <li className="DealSection__timer-item">
-                        <div className="DealSection__timer-number">57</div>
-                        <div className="DealSection__timer-period">sec</div>
-                    </li>
-                </ul>
-                <div className="DealSection__name-section">
-                    <h5 className="DealSection__subtitle">Propel Advanced Pro Frameset</h5>
-                    <ul className="DealSection__stars">
-                        <li className="DealSection__star-item">
-                            <i className="fas fa-star"/>
+class DealSection extends React.PureComponent {
+    render() {
+        return (
+            <section className="DealSection">
+                <div className="DealSection__image"/>
+                <div className="DealSection__info-block">
+                    <ul className="DealSection__timer-list">
+                        <li className="DealSection__timer-item">
+                            <div className="DealSection__timer-number">120</div>
+                            <div className="DealSection__timer-period">days</div>
                         </li>
-                        <li className="DealSection__star-item">
-                            <i className="fas fa-star"/>
+                        <li className="DealSection__timer-item">
+                            <div className="DealSection__timer-number">09</div>
+                            <div className="DealSection__timer-period">hour</div>
                         </li>
-                        <li className="DealSection__star-item">
-                            <i className="fas fa-star"/>
+                        <li className="DealSection__timer-item">
+                            <div className="DealSection__timer-number">38</div>
+                            <div className="DealSection__timer-period">min</div>
                         </li>
-                        <li className="DealSection__star-item">
-                            <i className="fas fa-star"/>
-                        </li>
-                        <li className="DealSection__star-item">
-                            <i className="fas fa-star-half-alt"/>
+                        <li className="DealSection__timer-item">
+                            <div className="DealSection__timer-number">57</div>
+                            <div className="DealSection__timer-period">sec</div>
                         </li>
                     </ul>
-                    <div className="DealSection__cost">$<span>2,290.00</span></div>
-                </div>
-                <div className="DealSection__color-stock">
-                    <div className="DealSection__color-pager">
-                        <div className="DealSection__color">Color:</div>
-                        <ul className="DealSection__pager">
-                            <li className="DealSection__pager-button DealSection__pager-button_1"/>
-                            <li className="DealSection__pager-button DealSection__pager-button_2"/>
-                            <li className="DealSection__pager-button DealSection__pager-button_3"/>
-                            <li className="DealSection__pager-button DealSection__pager-button_4"/>
-                            <li className="DealSection__pager-button DealSection__pager-button_5"/>
+                    <div className="DealSection__name-section">
+                        <h5 className="DealSection__subtitle">Propel Advanced Pro Frameset</h5>
+                        <ul className="DealSection__stars">
+                            <li className="DealSection__star-item">
+                                <i className="fas fa-star"/>
+                            </li>
+                            <li className="DealSection__star-item">
+                                <i className="fas fa-star"/>
+                            </li>
+                            <li className="DealSection__star-item">
+                                <i className="fas fa-star"/>
+                            </li>
+                            <li className="DealSection__star-item">
+                                <i className="fas fa-star"/>
+                            </li>
+                            <li className="DealSection__star-item">
+                                <i className="fas fa-star-half-alt"/>
+                            </li>
                         </ul>
+                        <div className="DealSection__cost">$<span>2,290.00</span></div>
                     </div>
-                    <div className="DealSection__in-stock">In stock: <span>10</span></div>
-                </div>
-                <div className="DealSection__button-block">
-                    <button type="button" className="DealSection__button DealSection__button_cart">
+                    <div className="DealSection__color-stock">
+                        <div className="DealSection__color-pager">
+                            <div className="DealSection__color">Color:</div>
+                            <ul className="DealSection__pager">
+                                <li className="DealSection__pager-button DealSection__pager-button_1"/>
+                                <li className="DealSection__pager-button DealSection__pager-button_2"/>
+                                <li className="DealSection__pager-button DealSection__pager-button_3"/>
+                                <li className="DealSection__pager-button DealSection__pager-button_4"/>
+                                <li className="DealSection__pager-button DealSection__pager-button_5"/>
+                            </ul>
+                        </div>
+                        <div className="DealSection__in-stock">In stock: <span>10</span></div>
+                    </div>
+                    <div className="DealSection__button-block">
+                        <button type="button" className="DealSection__button DealSection__button_cart">
                            <span className="DealSection__button-symbol">
                         <i className="fas fa-shopping-bag"/>
                         </span>
-                        add to cart
-                    </button>
-                    <button type="button" className="DealSection__button">
+                            add to cart
+                        </button>
+                        <button type="button" className="DealSection__button">
                         <span className="DealSection__button-symbol">
                         <i className="far fa-heart"/>
                         </span>
-                        add to wishlist
-                    </button>
+                            add to wishlist
+                        </button>
+                    </div>
                 </div>
-            </div>
-        </section>
-    );
-};
+            </section>
+        );
+    }
+}
 
 export default DealSection;
